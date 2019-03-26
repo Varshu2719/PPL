1efc37a154afa1c5b93d74390ec70cd413d6c2b2
Varshu2719 <44427307+Varshu2719@users.noreply.github.com>
2019-03-26 12:00:33 +0530

    Delete week1-1.cpp

--- week1-1.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int number;
-	printf("Enter a number: ");
-	scanf("%d",&number);
-	number%2==0 ? printf("%d is an even number",number) : printf("%d is an odd number",number);
-	return 0;
-}
